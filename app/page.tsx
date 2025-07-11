5f901b1e2089c47fb9e5ba64bb39be7006c53964
김규빈 <inkblot626@gmail.com>
2025-07-12 01:28:00 +0900

    setup and fetching

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Home",
+};
+
+export const API_URL = "https://billions-api.nomadcoders.workers.dev/";
+
+async function getBillions() {
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Home() {
+  const billions = await getBillions();
+  console.log(billions);
+  return (
+    <div className={styles.page}>
+      {billions.map((billion: any) => {
+        return (
+          <li>
+            <Link href={`/person/${billion.id}`}>{billion.name}111</Link>
+          </li>
+        );
+      })}
+    </div>
+  );
+}
